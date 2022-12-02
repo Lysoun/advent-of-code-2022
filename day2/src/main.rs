21b68c760c307cfbbc85f14d96e27328fa072250
Élise Rubio <elise.rubio@4sh.fr>
2022-12-02 21:36:45 +0100

    :sparkles: Part 2 day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,13 +1,5 @@
 use std::fs;
 
-fn convert_my_play_to_u32(play: char) -> u32 {
-    return match play {
-        'X' => 1,
-        'Y' => 2,
-        _ => 3
-    }
-}
-
 fn convert_opponent_play_to_u32(play: char) -> u32 {
     return match play {
         'A' => 1,
@@ -16,19 +8,22 @@ fn convert_opponent_play_to_u32(play: char) -> u32 {
     }
 }
 
-fn compute_score(my_play:char, opponent_play: char) -> u32 {
-    let my_play_u32 = convert_my_play_to_u32(my_play);
+fn compute_score(round_result: char, opponent_play: char) -> u32 {
     let opponent_play_u32 = convert_opponent_play_to_u32(opponent_play);
-    let match_outcome: u32 = if (my_play_u32 + 1) % 3 + 1 == opponent_play_u32 {
-        6
-    } else {
-        if my_play_u32 == opponent_play_u32 {
-            3
-        } else {
-            0
+    println!("round result: {}", round_result);
+    return match round_result {
+        'X' => match opponent_play_u32 {
+            1 => 3,
+            2 => 1,
+            _3 => 2
+        }
+        'Y' => 3 + opponent_play_u32,
+        _ => 6 + match opponent_play_u32 {
+            1 => 2,
+            2 => 3,
+            _ => 1
         }
     };
-    return match_outcome + my_play_u32
 }
 
 fn main() {
@@ -42,8 +37,9 @@ fn main() {
         let mut chars = line.chars();
         let opponent_play = chars.next().unwrap();
         chars.next();
-        let my_play = chars.next().unwrap();
-        let line_score = compute_score(my_play, opponent_play);
+        let round_result = chars.next().unwrap();
+        let line_score = compute_score(round_result, opponent_play);
+        println!("Line score: {}", line_score);
         total_score += line_score
     }
     println!("Total score: {}", total_score);
