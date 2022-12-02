4597cc27481a30930371aad2ea031acdf9df0930
Élise Rubio <elise.rubio@4sh.fr>
2022-12-02 19:41:42 +0100

    :sparkles: Part 2 day 1

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ fn main() {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error)
     };
-    let mut max_calories: u32 = 0;
+    let mut top_3_max_calories = vec![0, 0, 0];
     let mut elf_calories_number: u32 = 0;
     for line in input.lines() {
         if line == "" {
-            max_calories = max(max_calories, elf_calories_number);
+            top_3_max_calories[0] = max(top_3_max_calories[0], elf_calories_number);
+            top_3_max_calories.sort();
             elf_calories_number = 0;
         } else {
             let line_calories_number: u32 = match line.parse() {
@@ -21,7 +22,7 @@ fn main() {
             elf_calories_number += line_calories_number;
         }
     }
-    max_calories = max(max_calories, elf_calories_number);
+    top_3_max_calories[0] = max(top_3_max_calories[0], elf_calories_number);
 
-    println!("max calories: {}", max_calories)
+    println!("max calories: {}", (top_3_max_calories[0] + top_3_max_calories[1] + top_3_max_calories[2]))
 }
