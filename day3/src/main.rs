4151c8dc7739191c345e9266ace67a0cebc75303
Élise Rubio <elise.rubio@4sh.fr>
2022-12-04 17:32:37 +0100

    :sparkles: Part 2 day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn item_type_to_priority(item_type: char) -> u32 {
+fn item_type_to_priority(item_type: &char) -> u32 {
     return match item_type {
         'a' => 1,
         'b' => 2,
@@ -28,18 +28,8 @@ fn item_type_to_priority(item_type: char) -> u32 {
         'x' => 24,
         'y' => 25,
         'z' => 26,
-        _ => item_type_to_priority(item_type.to_ascii_lowercase()) + 26
+        _ => item_type_to_priority(&item_type.to_ascii_lowercase()) + 26
     };
-}
-
-fn find_item_type_in_both_parts(first_part: &str, second_part: &str) -> char {
-    for c in first_part.chars() {
-        if second_part.contains(c) {
-            return c;
-        }
-    }
-
-    return 'a';
 }
 
 fn main() {
@@ -49,15 +39,28 @@ fn main() {
         Err(error) => panic!("Problem opening the file: {:?}", error)
     };
     let mut total_priority: u32 = 0;
+    let mut group_counter: u8 = 0;
+    let mut group_badges_possibilities: Vec<char> = vec![];
     for line in input.lines() {
-        let middle_index = line.len() / 2;
-        let first_part = line.get(0..middle_index);
-        let second_part = line.get(middle_index..line.len());
-        let item_type_in_both_parts = find_item_type_in_both_parts(
-            first_part.unwrap(),
-            second_part.unwrap(),
-        );
-        total_priority += item_type_to_priority(item_type_in_both_parts);
+        group_counter += 1;
+
+        if group_counter == 1 {
+            group_badges_possibilities = vec![];
+            for c in line.chars() {
+                group_badges_possibilities.push(c);
+            }
+        } else {
+            for i in (0..group_badges_possibilities.len()).rev() {
+                if !line.contains(&group_badges_possibilities.get(i).unwrap().to_string()) {
+                    group_badges_possibilities.remove(i);
+                }
+            }
+
+            if group_counter == 3 {
+                group_counter = 0;
+                total_priority += item_type_to_priority(group_badges_possibilities.get(0).unwrap());
+            }
+        }
     }
     println!("Total priority: {}", total_priority);
 }
