f17c71a7e4fcd33cde8d4311cfd845137c21fe2a
Élise Rubio <elise.rubio@4sh.fr>
2022-12-11 21:40:32 +0100

    :sparkles: Part 1 day 9

---
+++ day9/src/main.rs
@@ -0,0 +1,87 @@
+use std::cmp::max;
+use std::collections::HashSet;
+use std::fs;
+
+fn safe_file_read(path: &str) -> String {
+    return match fs::read_to_string(path) {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error)
+    };
+}
+
+fn compute_new_position(direction: char, position: (i16, i16)) -> (i16, i16) {
+    return match direction {
+        'U' => (position.0 + 1, position.1),
+        'D' => (position.0 - 1, position.1),
+        'L' => (position.0, position.1 - 1),
+        _ => (position.0, position.1 + 1)
+    };
+}
+
+fn compute_distance(pos1: (i16, i16), pos2: (i16, i16)) -> i16 {
+    return max(
+        (pos1.1 - pos2.1).abs(),
+        (pos1.0 - pos2.0).abs()
+    )
+}
+
+fn compute_new_tail_position(head_position: (i16, i16), tail_position: (i16, i16)) -> (i16, i16) {
+    if head_position.0 == tail_position.0 {
+        if head_position.1 > tail_position.1 {
+            return (head_position.0, head_position.1 - 1);
+        }
+        return  (head_position.0, head_position.1 + 1);
+    }
+
+    if head_position.1 == tail_position.1 {
+        if head_position.0 > tail_position.0 {
+            return (head_position.0 - 1, head_position.1);
+        }
+        return (head_position.0 + 1, head_position.1);
+    }
+
+    let mut movement: (i16, i16) = (0, 0);
+
+    if head_position.0 > tail_position.0 {
+        movement.0 = 1;
+    } else {
+        movement.0 = -1;
+    }
+
+    if head_position.1 > tail_position.1 {
+        movement.1 = 1;
+    } else {
+        movement.1 = -1;
+    }
+
+    return (tail_position.0 + movement.0, tail_position.1 + movement.1);
+}
+
+fn main() {
+    let input = safe_file_read("input.txt");
+    let input_lines = input.lines();
+    let mut head_position: (i16, i16) = (0, 0);
+    let mut tail_position: (i16, i16) = (0, 0);
+    let mut visited_positions: HashSet<(i16, i16)> = HashSet::new();
+    visited_positions.insert(tail_position);
+
+    for line in input_lines {
+        let line_split: Vec<&str> = line.split_whitespace().collect();
+        let direction = line_split[0].chars().next().unwrap();
+        let steps_number: i16 =  match line_split[1].parse() {
+            Ok(n) => n,
+            Err(error) =>  panic!("Problem reading line: {:?}", error)
+        };
+
+        for _i in 0..steps_number {
+            head_position = compute_new_position(direction, head_position);
+
+            if compute_distance(head_position, tail_position) > 1 {
+                tail_position = compute_new_tail_position(head_position, tail_position);
+                visited_positions.insert(tail_position);
+            }
+        }
+    }
+
+    println!("Number of visited positions by the rope tail: {}", visited_positions.len());
+}
