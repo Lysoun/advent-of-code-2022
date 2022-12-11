ba8a4971242830a55d33f1a2ac912ece1770d952
Élise Rubio <elise.rubio@4sh.fr>
2022-12-11 14:22:31 +0100

    :sparkles: Part 2 day 8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -60,6 +60,43 @@ fn is_tree_visible(tree: (&(u16, u16), &u8), trees: &HashMap<(u16, u16), u8>) ->
         is_tree_visible_on_column(tree_column_number + 1, grid_size, tree_line_number, &tree_height, trees);
 }
 
+fn compute_scenic_score(tree: (&(u16, u16), &u8), trees: &HashMap<(u16, u16), u8>) -> u32 {
+    let tree_line_number = tree.0.0;
+    let tree_column_number = tree.0.1;
+    let tree_height = tree.1;
+    let mut trees_visible: (u32, u32, u32, u32) = (0, 0, 0, 0);
+
+    for i in (0..tree_line_number).rev() {
+        trees_visible.0 += 1;
+        if trees.get(&(i, tree_column_number)).unwrap() >= tree_height {
+            break;
+        }
+    }
+
+    for i in (0..tree_column_number).rev() {
+        trees_visible.1 += 1;
+        if trees.get(&(tree_line_number, i)).unwrap() >= tree_height {
+            break;
+        }
+    }
+
+    for i in (tree_line_number + 1)..99 {
+        trees_visible.2 += 1;
+        if trees.get(&(i, tree_column_number)).unwrap() >= tree_height {
+            break;
+        }
+    }
+
+    for i in (tree_column_number + 1)..99 {
+        trees_visible.3 += 1;
+        if trees.get(&(tree_line_number, i)).unwrap() >= tree_height {
+            break;
+        }
+    }
+
+    return trees_visible.0 * trees_visible.1 * trees_visible.2 * trees_visible.3;
+}
+
 fn main() {
     let input = safe_file_read("input.txt");
     let mut trees: HashMap<(u16, u16), u8> = HashMap::new();
@@ -77,17 +114,20 @@ fn main() {
         line_number += 1;
     }
 
-    let mut number_of_visible_trees: u16 = 0;
     let mut iter = trees.iter();
+    let mut max_scenic_score: u32 = 0;
 
     let mut tree = iter.next();
     while tree.is_some() {
         let unwrapped_tree = tree.unwrap();
         if is_tree_visible(unwrapped_tree, &trees) {
-            number_of_visible_trees += 1;
+            let tree_scenic_score = compute_scenic_score(unwrapped_tree, &trees);
+            if tree_scenic_score > max_scenic_score {
+                max_scenic_score = tree_scenic_score;
+            }
         }
         tree = iter.next();
     }
 
-    println!("Number of visible trees: {}", number_of_visible_trees);
+    println!("Maximum scenic score: {}", max_scenic_score);
 }
